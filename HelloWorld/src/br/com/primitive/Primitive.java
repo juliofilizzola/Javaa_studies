32cc933ee6325fcbf405fdc86e30183908960c5c
Filizzola <juliofilizzola@hotmail.com>
2021-12-02 14:44:19 -0300

    Adicionando comentarios e imprimindo outras váriavies

--- HelloWorld/src/br/com/primitive/Primitive.java
+++ HelloWorld/src/br/com/primitive/Primitive.java
@@ -2,11 +2,13 @@ package br.com.primitive;
 
 public class Primitive {
   public static void main(String[] args) {
-    byte byteVar = 127;
-    short shortVar = 10000;
-    int intVar = 1000000000;
+    byte byteVar = 127; // Valor maximo do byte;
+    short shortVar = 32767; // Valor máximo do short;
+    int intVar = 2147483647; // Valor máximo do int;
+    long longVar = 9223372036854770000L; // valor máximo do long;
     System.out.println(byteVar);
     System.out.println(shortVar);
     System.out.println(intVar);
+    System.out.println(longVar);
   }
 }
