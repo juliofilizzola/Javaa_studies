58988138bc7cb40a9794481fed64ee0385107268
Filizzola <juliofilizzola@hotmail.com>
2021-12-02 17:21:41 -0300

    Build: comentado os valores primitivos imprimindo no principal

--- HelloWorld/src/br/com/Principal.java
+++ HelloWorld/src/br/com/Principal.java
@@ -1,8 +1,13 @@
 package br.com;
 
+import br.com.primitive.Primitive;
+
 public class Principal {
   public static void main(String[] args) {
+    Primitive primitive = new Primitive();
     String Hello = "Hello World";
     System.out.println(Hello);
+    System.out.println(primitive.longVar);
+
   }
 }
--- HelloWorld/src/br/com/primitive/Primitive.java
+++ HelloWorld/src/br/com/primitive/Primitive.java
@@ -1,24 +1,25 @@
 package br.com.primitive;
 
 public class Primitive {
-  public static void main(String[] args) {
+
+//  public static void main(String[] args) {
     byte byteVar = 127; // Valor maximo do byte;
     short shortVar = 32767; // Valor máximo do short;
     int intVar = 2147483647; // Valor máximo do int;
-    long longVar = 9223372036854770000L; // valor máximo do long;
+    public long longVar = 9223372036854770000L; // valor máximo do long;
 
     float floatVar = 10.10F; // valor máximo relativo do float;
     double doubleVar = 10000000000.299; // valor maximo relativo do double;
     char charVar = 's'; // valor máximo do char;
     boolean booleanVar = true; //valor possivel do boolean;
 
-    System.out.println(byteVar);
-    System.out.println(shortVar);
-    System.out.println(intVar);
-    System.out.println(longVar);
-    System.out.println(floatVar);
-    System.out.println(doubleVar);
-    System.out.println(charVar);
-    System.out.println(booleanVar);
-  }
+//    System.out.println(byteVar);
+//    System.out.println(shortVar);
+//    System.out.println(intVar);
+//    System.out.println(longVar);
+//    System.out.println(floatVar);
+//    System.out.println(doubleVar);
+//    System.out.println(charVar);
+//    System.out.println(booleanVar);
+//  }
 }
