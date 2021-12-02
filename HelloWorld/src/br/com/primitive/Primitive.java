3db76509e1be8d52a0e82a38117569b77db56809
Filizzola <juliofilizzola@hotmail.com>
2021-12-02 16:33:41 -0300

    Build: escrevendo outras variavies (float)

--- HelloWorld/src/br/com/primitive/Primitive.java
+++ HelloWorld/src/br/com/primitive/Primitive.java
@@ -6,9 +6,12 @@ public class Primitive {
     short shortVar = 32767; // Valor máximo do short;
     int intVar = 2147483647; // Valor máximo do int;
     long longVar = 9223372036854770000L; // valor máximo do long;
+
+    float floatVar = 10.10F;
     System.out.println(byteVar);
     System.out.println(shortVar);
     System.out.println(intVar);
     System.out.println(longVar);
+    System.out.println(floatVar);
   }
 }
