bddc2c2b5f0d8eb1033484ae30ac2d2743800568
Filizzola <juliofilizzola@hotmail.com>
2021-12-02 13:50:30 -0300

    Criando variaveis primitivas no JAVA e imprimindo.

---
+++ HelloWorld/src/br/com/Principal.java
@@ -0,0 +1,8 @@
+package br.com;
+
+public class Principal {
+  public static void main(String[] args) {
+    String Hello = "Hello World";
+    System.out.println(Hello);
+  }
+}
---
+++ HelloWorld/src/br/com/primitive/Primitive.java
@@ -0,0 +1,12 @@
+package br.com.primitive;
+
+public class Primitive {
+  public static void main(String[] args) {
+    byte byteVar = 127;
+    short shortVar = 10000;
+    int intVar = 1000000000;
+    System.out.println(byteVar);
+    System.out.println(shortVar);
+    System.out.println(intVar);
+  }
+}
