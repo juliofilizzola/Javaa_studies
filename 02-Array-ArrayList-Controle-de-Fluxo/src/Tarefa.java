b7df8cc77735810f1cff03af1dd5d7376cc7623e
Filizzola <juliofilizzola@hotmail.com>
2021-12-10 08:40:55 -0300

    BUILD: criando metodos exibirTarefa, Tarefa e obterTamanhoTarefa

--- 02-Array-ArrayList-Controle-de-Fluxo/src/Tarefa.java
+++ 02-Array-ArrayList-Controle-de-Fluxo/src/Tarefa.java
@@ -1,2 +1,15 @@
 public class Tarefa {
+    String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void exibirTtarefa() {
+        System.out.println(descricao);
+    }
+
+    public int obterTamanhotarefa() {
+        return descricao.length();
+    }
 }
