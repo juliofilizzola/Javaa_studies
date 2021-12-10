ddc55e21757b1233ba597c23b6de5569e67daf4b
Filizzola <juliofilizzola@hotmail.com>
2021-12-10 08:49:15 -0300

    BUILD: executando metodos

--- 02-Array-ArrayList-Controle-de-Fluxo/src/br/com/Principal.java
+++ 02-Array-ArrayList-Controle-de-Fluxo/src/br/com/Principal.java
@@ -1,7 +1,13 @@
 package br.com;
 
+import br.com.tarefas.Tarefa;
+
 public class Principal {
     public static void main(String[] args) {
-
+        System.out.println("Minhas Tarefas");
+        System.out.println("----------------------");
+        Tarefa tarefa1 = new Tarefa("Regar as Plantas");
+        tarefa1.exibirTtarefa();
+        System.out.println("A tarefa tem " + tarefa1.obterTamanhotarefa() + " caracterers");
     }
 }
