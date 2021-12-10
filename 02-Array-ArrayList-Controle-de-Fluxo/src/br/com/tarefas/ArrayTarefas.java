aaaa83026b0d1afce717143c8df8689b46125add
Filizzola <juliofilizzola@hotmail.com>
2021-12-10 10:58:23 -0300

    BUILD: criando metodos para adiconar uma nova tarefa ao array"

--- 02-Array-ArrayList-Controle-de-Fluxo/src/br/com/tarefas/ArrayTarefas.java
+++ 02-Array-ArrayList-Controle-de-Fluxo/src/br/com/tarefas/ArrayTarefas.java
@@ -1,4 +1,19 @@
 package br.com.tarefas;
 
 public class ArrayTarefas {
+
+    Tarefa[] tarefas;
+
+    public ArrayTarefas(int tamanho) {
+        tarefas = new Tarefa[tamanho];
+    }
+
+    public void adcionar (Tarefa tarefa) {
+        tarefas[0] = tarefa;
+    }
+
+    public void removerTarefa(int posicao) {
+        tarefas[posicao] = null;
+    }
+
 }
