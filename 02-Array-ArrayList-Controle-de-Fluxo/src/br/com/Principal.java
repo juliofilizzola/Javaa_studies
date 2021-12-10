b8ef6cad5981a0e820267c1ed67dbde9911f43b8
Filizzola <juliofilizzola@hotmail.com>
2021-12-10 12:08:41 -0300

    BUILD: importando listArray

--- 02-Array-ArrayList-Controle-de-Fluxo/src/br/com/Principal.java
+++ 02-Array-ArrayList-Controle-de-Fluxo/src/br/com/Principal.java
@@ -1,19 +1,26 @@
 package br.com;
 
-import br.com.tarefas.ArrayTarefas;
+
+import br.com.tarefas.ListaTarefas;
 import br.com.tarefas.Tarefa;
 
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Minhas Tarefas");
         System.out.println("----------------------");
-        ArrayTarefas tarefas = new ArrayTarefas(3);
+
+        ListaTarefas tarefas = new ListaTarefas();
+
         Tarefa tarefa1 = new Tarefa("Xablau");
         Tarefa tarefa2 = new Tarefa("bora estudar");
         Tarefa tarefa3 = new Tarefa("bora trabalhar");
+        Tarefa tarefa4 = new Tarefa("bora trabalhar VAMOS");
+
         tarefas.adicionar(tarefa1);
         tarefas.adicionar(tarefa2);
         tarefas.adicionar(tarefa3);
+        tarefas.adicionar(tarefa4);
+
         tarefas.exibirTarefas();
     }
 }
