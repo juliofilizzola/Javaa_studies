592c787c47c0be1381f13bd877071367df5849cc
Filizzola <juliofilizzola@hotmail.com>
2021-12-25 09:16:38 -0300

    🌐: criando class tv e também interface ControleRemoto

---
+++ 03-POO/src/poo/classTV/TV.java
@@ -0,0 +1,16 @@
+package poo.classTV;
+
+public class TV {
+    private int tamanho;
+    private int canal;
+    private int volume;
+    private boolean ligada;
+
+    public TV(int tamanho) {
+        this.tamanho = tamanho;
+        this.canal = canal;
+        this.volume = volume;
+        this.ligada = false;
+    }
+}
+
