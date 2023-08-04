6fb7cdfacbf53d65199fa0a0011d5a4e350bb0d0
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-08-04 20:14:14 -0300

    feat: init example

---
+++ trybe/Pensionado/src/entities/Rent.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Rent {
+
+    private String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+
+}
