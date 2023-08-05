4b8c0ffd594179a84a49dfe9e6a1374a27ffd1d0
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-08-05 19:30:18 -0300

    feat: finished

---
+++ exercise/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Exercise!");
+		Locale.setDefault(Locale.US);
+	}
+	public void employees() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("how a may employees wil be rented?");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i ++) {
+		
+		}
+	}
+}
